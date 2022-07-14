c3f8700ecc408a34576330b0451b2a3e4329a588
LordCasser <591289332@qq.com>
2022-07-14 23:22:12 +0800

    add tls support

--- main.go
+++ main.go
@@ -19,8 +19,22 @@ func prepareTerminfo() string {
 	return dir
 }
 
+func Redirect(w http.ResponseWriter, req *http.Request) {
+	// remove/add not default ports from req.Host
+	target := "https://" + req.Host + req.URL.Path
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+	log.Printf("nofound to: %s", target)
+	http.Redirect(w, req, target,
+		// see comments below and consider the codes 308, 302, or 301
+		http.StatusTemporaryRedirect)
+}
+
 func main() {
 	port := flag.Int("p", 80, "port that http serve on")
+	SSL := flag.Bool("SSL", false, "open SSL or not, default is true")
+
 	flag.Parse()
 	cmdDesc := flag.Args()
 
@@ -37,8 +51,21 @@ func main() {
 			return cmd
 		},
 	})
-	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), tty)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	portString := fmt.Sprintf(":%d", *port)
+	_, crtErr := os.Stat("https.crt")
+	_, keyErr := os.Stat("https.key")
+	if *SSL && (crtErr == nil && keyErr == nil) {
+		if *port == 0 {
+			portString = fmt.Sprintf(":%d", 443)
+		}
+		go func() {
+			http.ListenAndServe(":80", http.HandlerFunc(Redirect))
+		}()
+		log.Fatal(http.ListenAndServeTLS(portString, "tls.crt", "tls.key", tty))
+	} else {
+		if *port == 0 {
+			portString = fmt.Sprintf(":%d", 80)
+		}
+		log.Fatal(http.ListenAndServe(portString, tty))
 	}
 }
